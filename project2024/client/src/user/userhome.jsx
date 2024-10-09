ce87520d2d931f60eac9a062eca7afdc5a59d9a3
adithyank21 <adithyank21@gmail.com.com>
2024-10-09 10:28:39 +0530

    first commit

---
+++ project2024/client/src/user/userhome.jsx
@@ -0,0 +1,18 @@
+
+import {Box,Paper} from "@mui/material"
+import Grid from '@mui/material/Grid2';
+function UserHome(){
+    return(
+        <>
+      <Box>
+        <Grid container spacing={2}>
+            <Grid size={4}>
+                <Paper>size 4</Paper>
+            </Grid>
+        </Grid>
+      </Box>
+        </>
+    )
+    
+}
+export default UserHome;
